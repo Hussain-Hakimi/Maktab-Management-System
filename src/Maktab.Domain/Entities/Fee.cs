6bfebf9b6467e0d1a03d40b6c599f61f54d5b848
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-08-19 22:33:13 +0500

    Create Fee.cs

---
+++ src/Maktab.Domain/Entities/Fee.cs
@@ -0,0 +1,11 @@
+namespace Maktab.Domain.Entities;
+
+public sealed class Fee
+{
+    public int FeeId { get; set; }
+    public int StudentId { get; set; }
+    public required string FeeType { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
