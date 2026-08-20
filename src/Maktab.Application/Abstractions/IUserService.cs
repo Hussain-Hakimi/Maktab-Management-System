4b1c723b18c8bc22976106fb5018d25b3a60a88d
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-08-20 13:24:48 +0500

    Create IUserService.cs

---
+++ src/Maktab.Application/Abstractions/IUserService.cs
@@ -0,0 +1,10 @@
+namespace Maktab.Application.Abstractions;
+
+public interface IUserService
+{
+    Task<UserDto?> AuthenticateAsync(LoginDto login, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<UserDto>> GetAllUsersAsync(CancellationToken cancellationToken = default);
+    Task<int> CreateUserAsync(SaveUserDto user, CancellationToken cancellationToken = default);
+    Task UpdateUserAsync(int userId, SaveUserDto user, CancellationToken cancellationToken = default);
+    Task DeleteUserAsync(int userId, CancellationToken cancellationToken = default);
+}
