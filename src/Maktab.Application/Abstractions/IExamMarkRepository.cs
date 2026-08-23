f2682b45aae8dab0e697833c3f6b4bd238d9b5d2
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-08-23 13:17:32 +0500

    Update IExamMarkRepository.cs

--- src/Maktab.Application/Abstractions/IExamMarkRepository.cs
+++ src/Maktab.Application/Abstractions/IExamMarkRepository.cs
@@ -6,6 +6,7 @@ public interface IExamMarkRepository
 {
     Task<IReadOnlyList<ExamMark>> GetMarksByClassAndSubjectAsync(int classId, int subjectId, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<ExamMark>> GetMarksByStudentAsync(int studentId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<ExamMark>> GetMarksByStudentAndYearAsync(int studentId, int academicYearId, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<ExamMark>> GetMarksByClassAsync(int classId, CancellationToken cancellationToken = default);
     Task SaveOrUpdateMarkAsync(ExamMark mark, CancellationToken cancellationToken = default);
     Task SaveOrUpdateMarksBatchAsync(IEnumerable<ExamMark> marks, CancellationToken cancellationToken = default);
